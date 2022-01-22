30b5cb6bdc33c00398fb60201d2c0d36ce226c31
Groofidze <groofi@list.ru>
2022-01-22 17:07:24 +0300

    Home_4 v 1.1

--- parse.py
+++ parse.py
@@ -2,20 +2,23 @@ from lxml import html
 import requests
 from pprint import pprint
 
+from pymongo import MongoClient
+
 url = 'https://lenta.ru/'
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
                          '(KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'}
 
-response = requests.get(url, headers=headers)
+client = MongoClient("127.0.0.1", 27017)["Lenta"]
+lenta = client.lenta
 
+response = requests.get(url, headers=headers)
 dom = html.fromstring(response.text)
 
-drones = []
 
 items = dom.xpath("//div[@class='card-big _topnews _news'] | //a[@class='card-mini _topnews']")
 print(len(items))
-for item in items:
+for number, item in enumerate(items):
     drone = {}
     name = item.xpath(".//text()")
     link = item.xpath("./@href")
@@ -27,6 +30,7 @@ for item in items:
     drone['data'] = data
     drone['source'] = "lenta.ru"
 
-    drones.append(drone)
+    lenta.insert_one(drone)
 
-pprint(drones)
+
+print(lenta.count_documents({}))
