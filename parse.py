c9f8e86467b114182b5fceb3fa930b2dbddfc5b8
Groofidze <groofi@list.ru>
2022-01-22 17:19:53 +0300

    Home_4 v 1.3

--- parse.py
+++ parse.py
@@ -15,7 +15,6 @@ lenta = client.lenta
 response = requests.get(url, headers=headers)
 dom = html.fromstring(response.text)
 
-
 items = dom.xpath("//div[@class='card-big _topnews _news'] | //a[@class='card-mini _topnews']")
 print(len(items))
 for number, item in enumerate(items):
@@ -25,6 +24,7 @@ for number, item in enumerate(items):
     time = item.xpath(".//time/text()")
     data = f"{'-'.join(link[0].split('/')[2:5])} {time[0]}"
 
+    drone["_id"] = number
     drone['name'] = name[0]
     drone['link'] = f"{url}{link[0]}"
     drone['data'] = data
